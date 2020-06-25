f6cb5111dcba38f6c43172910be26b20a44cdf4e
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-06-24 21:40:50 -0400

    adding models

---
+++ src/app/models/job.ts
@@ -0,0 +1,15 @@
+import { Category } from "./category";
+
+export class Job{
+    company: string;
+    category: Category;
+    type: string;
+    url: string;
+    position: string;
+    location: string;
+    email: string;
+    description: string;
+    logo: string;
+    date: Date;
+
+}
