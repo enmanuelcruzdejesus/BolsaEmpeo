c9ab761ed37d9f258ad1218aa64fb226ee8b80df
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-07-05 22:17:54 -0400

    post-job using form data

--- src/app/jobs/post-job.component.ts
+++ src/app/jobs/post-job.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
 import { Job } from '../models/job';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-post-job',
@@ -11,19 +12,41 @@ import { Job } from '../models/job';
 export class PostJobComponent implements OnInit {
 
   
-  job = new Job();
-    public response  : any = null;
+    job = new Job();
+    categories: Category[];
+    response  : any = null;
+
 
   constructor(private service: DataService, private router: Router) { }
 
   ngOnInit(): void {
+    this.service.getCategory().subscribe((res)=>{
+      this.categories = res;
+      this.categories.forEach((v)=>{
+          console.log(v);
+      });
+    },(err)=>{
+      console.error(err);
+    });
   }
 
   postJob() : void{
-      
-    this.service.createJob(this.job).subscribe((res)=>{
+    
+    var formdata = new FormData();
+    formdata.append("company",this.job.company);
+    formdata.append("category",this.job.category);
+    formdata.append("type",this.job.type);
+    formdata.append("url",this.job.url);
+    formdata.append("position",this.job.position);
+    formdata.append("location",this.job.location);
+    formdata.append("compemail",this.job.email);
+    formdata.append("description",this.job.description);
+
+
+    this.service.createJob(formdata).subscribe((res)=>{
      
      this.response = res;
+     
      
      this.router.navigate(['/jobs']);
    },(err)=>{
